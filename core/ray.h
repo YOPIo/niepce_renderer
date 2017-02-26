41198c7d7196ab8e0e8b1e7d4bb7a2de4418b882
YOPIo <yopio.egg@gmail.com>
2017-02-26 16:41:53 +0900

    add ray class

--- core/ray.h
+++ core/ray.h
@@ -11,26 +11,46 @@ namespace niepce
 class Ray
 {
  public:
-  Ray(){}
-  virtual ~Ray(){}
+  Ray();
+  virtual ~Ray();
+
   Ray(const Ray& ray) = default;
   Ray(Ray&& ray)      = default;
+
   Ray& operator = (const Ray& ray) = default;
   Ray& operator = (Ray&& ray)      = default;
 
-  auto operator () (Float t) const -> Point3f
-  {
-    return origin + direction * t;
-  }
-  auto HasNaNs() const -> bool
-  {
-    return origin.HasNaNs() || direction.HasNaNs();
-  }
+  auto operator () (Float t) const -> Point3f;
+  auto HasNaNs() const -> bool;
 
  public:
   Point3f  origin;
   Vector3f direction;
 };
+
+class RayDifferential : public Ray
+{
+ public:
+  RayDifferential();
+  virtual ~RayDifferential();
+
+  RayDifferential(const RayDifferential& ray) = default;
+  RayDifferential(RayDifferential&& ray)      = default;
+
+  RayDifferential& operator = (const RayDifferential& ray) = default;
+  RayDifferential& operator = (RayDifferential&& ray)      = default;
+
+  auto ScaleDifferential(Float t) -> void;
+  auto HasNaNs() const -> bool;
+
+ public:
+  bool     has_differential;
+  Point3f  rx_origin,    ry_origin;
+  Vector3f rx_direction, ry_direction;
+};
+
+auto operator << (std::ostream& os, const Ray& ray) -> std::ostream&;
+auto operator << (std::ostream& os, const RayDifferential& ray) -> std::ostream&;
 
 } // namespace niepce
 
