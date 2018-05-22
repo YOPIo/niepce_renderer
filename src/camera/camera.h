f4a3099486ca3311b37e188716cf393be94c144f
yopio <s1210228@gmail.com>
2018-05-22 02:52:21 +0000

    Add camera class

---
+++ src/camera/camera.h
@@ -0,0 +1,72 @@
+/*!
+ * @file camera.h
+ * @brief 
+ * @author Masashi Yoshida
+ * @date 
+ * @details 
+ */
+#ifndef _CAMERA_H_
+#define _CAMERA_H_
+/*
+// ---------------------------------------------------------------------------
+*/
+#include "../core/niepce.h"
+#include "../core/transform.h"
+/*
+// ---------------------------------------------------------------------------
+*/
+namespace niepce
+{
+//! ----------------------------------------------------------------------------
+//! @class Camera
+//! @brief
+//! @details
+//! ----------------------------------------------------------------------------
+class Camera
+{
+public:
+  //! The default class constructor.
+  Camera () = default;
+
+  //! The constructor takes transform matrix.
+  Camera (const Transform& t);
+
+  //! The copy constructor of the class.
+  Camera (const Camera& camera) = default;
+
+  //! The move constructor of the class.
+  Camera (Camera&& camera) = default;
+
+  //! The default class destructor.
+  virtual ~Camera () = default;
+
+  //! The copy assignment operator of the class.
+  auto operator = (const Camera& camera) -> Camera& = default;
+
+  //! The move assignment operator of the class.
+  auto operator = (Camera&& camera) -> Camera& = default;
+
+public:
+  /*!
+   * @fn Ray GenerateRay ()
+   * @brief Generate the ray from camera.
+   * @return 
+   * @exception none
+   * @details
+   */
+  virtual auto GenerateRay () const -> Ray = 0;
+
+private:
+  /*!
+   * @brief Matrix that transform camera coordinate to world coordinates.
+   */
+  Transform camera_to_world_;
+}; // class Camera
+/*
+// ---------------------------------------------------------------------------
+*/
+}  // namespace niepce
+/*
+// ---------------------------------------------------------------------------
+*/
+#endif // _CAMERA_H_
